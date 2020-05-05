0800d3abcc5c88b7afc569de5b6a3831b0be975d
Nick Stevens <nick.stevens@smartthings.com>
2020-05-05 12:50:19 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use futures::future::{self, FutureExt};
+use hyper::{Client, Uri};
+
+#[actix_rt::main]
+async fn main() {
+    let n = std::env::args().nth(1).map(|s| s.parse::<usize>().unwrap()).unwrap_or(100);
+    println!("Running with {} futures", n);
+    hang(n).await;
+}
+
+async fn hang(n: usize) {
+    env_logger::init();
+    let mut futures = Vec::new();
+    let client = Client::new();
+    // let request = client.get(Uri::from_static("http://localhost:8000/test_file.bin"));
+    let request = client.get(Uri::from_static("http://www.google.com"));
+    let fut = async move { request.await.unwrap(); }.shared();
+    for _ in 0..n {
+        futures.push(fut.clone());
+    }
+    futures.push(fut);
+    future::join_all(futures).await;
+}
