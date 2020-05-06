a77b4d29f4a6c16dfd17dd473d8d9dadc1364993
Nick Stevens <nick.stevens@smartthings.com>
2020-05-06 12:29:00 -0500

    New way to hang things

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
-use futures::future::{self, FutureExt};
-use hyper::{Client, Uri};
+use futures::future::FutureExt;
+use futures::stream::{StreamExt, FuturesUnordered};
+use std::fs;
 
-#[tokio::main]
-async fn main() {
+fn main() {
+    env_logger::init();
     let n = std::env::args().nth(1).map(|s| s.parse::<usize>().unwrap()).unwrap_or(100);
     println!("Running with {} futures", n);
-    hang(n).await;
+    let mut rt = tokio::runtime::Builder::new()
+        .enable_all()
+        .threaded_scheduler()
+        .max_threads(64)
+        .build()
+        .unwrap();
+    rt.block_on(async move {
+        let mut multiple = FuturesUnordered::new();
+        for _ in 0..n {
+            multiple.push(hang(n));
+        }
+        while let Some(_) = multiple.next().await {
+            println!("Outer");
+        }
+    });
 }
 
 async fn hang(n: usize) {
-    env_logger::init();
-    let mut futures = Vec::new();
-    let client = Client::new();
-    // let request = client.get(Uri::from_static("http://localhost:8000/test_file.bin"));
-    let request = client.get(Uri::from_static("http://www.google.com"));
-    let fut = async move { request.await.unwrap(); }.shared();
+    let mut futures = FuturesUnordered::new();
+    let fut = async move { 
+        tokio::task::spawn_blocking(move || {
+            fs::read("test_file.bin").map(Vec::into_boxed_slice).unwrap()
+        }).await.unwrap()
+    }.shared();
     for _ in 0..n {
         futures.push(fut.clone());
     }
     futures.push(fut);
-    future::join_all(futures).await;
+    while let Some(_) = futures.next().await {
+        println!("Inner");
+    }
 }
