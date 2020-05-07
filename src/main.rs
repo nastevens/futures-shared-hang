4b21c75990a9c1cab84595582ace8dee004724ab
Nick Stevens <nick.stevens@smartthings.com>
2020-05-06 22:04:25 -0500

    Minimum case

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ use std::future::Future;
 
 #[tokio::main]
 async fn main() {
-    let n = std::env::args().nth(1).map(|s| s.parse::<usize>().unwrap()).unwrap_or(1);
+    let n = std::env::args().nth(1).map(|s| s.parse::<usize>().unwrap()).unwrap_or(129);
     println!("Running with {} futures", n);
+
     let mut multiple = FuturesUnordered::new();
     for _ in 0..n {
         multiple.push(shared());
     }
-    while let Some(_) = multiple.next().await {}
+
+    while let Some(_) = multiple.next().await {
+        // Adding this line will fix the issue
+        // tokio::task::yield_now().await;
+    }
 }
 
 fn shared() -> impl Future<Output = ()> {
-    tokio::spawn(async {}).then(|_| async {}).shared()
+    let join_handle = tokio::spawn(async {}).then(|_| async {});
+    join_handle.shared()
 }
